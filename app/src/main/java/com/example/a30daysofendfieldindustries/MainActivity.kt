f8d3345efde7f4aa80334826864c7838a0f39985
Haveiszal Faroqie <hvszal@gmail.com>
2026-02-22 11:51:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a30daysofendfieldindustries/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.a30daysofendfieldindustries
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.a30daysofendfieldindustries.ui.theme._30DaysOfEndfieldIndustriesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _30DaysOfEndfieldIndustriesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    _30DaysOfEndfieldIndustriesTheme {
+        Greeting("Android")
+    }
+}
